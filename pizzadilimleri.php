489204c321225df4fe28e75dda84ca0ceb8a5af8
Yasin KARABULAK <yasinkarabulak@gmail.com>
2019-05-03 16:47:07 +0300

    8 dilimilik yeni pizza yapıldı.

--- pizzadilimleri.php
+++ pizzadilimleri.php
@@ -6,4 +6,13 @@ $dilimler = explode(" ", $pizza);
 echo $dilimler[0]; // dilim1
 echo $dilimler[1]; // dilim2
 
+$pizzaYeni = array();
+
+foreach (range(1,8) as $dilim) {
+    $pizzaYeni[] = "Dilim-" . $dilim;
+}
+
+echo "Yeni pizza toplam " . count($pizzaYeni) . " dilimdir.";
+print_r($pizzaYeni);
+
 ?>
